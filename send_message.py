9cd96ce19348dc9dff912e4bd12ee6264070d839
Ensar Gök <ensarxgok@gmail.com>
2023-07-04 15:23:58 +0300

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -8,10 +8,22 @@ def lambda_handler(event, context):
     connectionId = event['requestContext']['connectionId']
     user_name = event.get('queryStringParameters', {'name': 'guest'}).get('name')
     
-    to_send = "Sulin"
+    to_send = json.loads(event['body']).get('to_send', None)
+    message = json.loads(event['body']).get('message', None)
+
+    if not message:
+        return {"statusCode": 400, "body": "Missing parameter 'message'"}
     
+    if not to_send:
+        return {"statusCode": 400, "body": "Missing parameter 'to_send'"}
+
     dynamo = boto3.resource('dynamodb').Table(os.environ['TABLE_NAME'])
     resultU = dynamo.scan(FilterExpression=Key('user_name').eq(to_send))
+
+    result_count = resultU['Count']
+    scaned_colunt = resultU['ScannedCount']
+
+    json_data = {"Message":{"from":user_name,"data":message},"result_count": result_count, "scaned_colunt": scaned_colunt}
     
     receiver_id = resultU['Items'][0]['connectionId']
     
@@ -23,11 +35,11 @@ def lambda_handler(event, context):
     domain = event["requestContext"]["domainName"]
     stage = event["requestContext"]["stage"]
     apig_management_client = boto3.client('apigatewaymanagementapi', endpoint_url=f'https://{domain}/{stage}')
-        
 
     logger.info("Trying to send")
+    logger.info(json_data)
 
-    apig_management_client.post_to_connection(Data="test", ConnectionId=receiver_id)
+    apig_management_client.post_to_connection(Data=json.dumps(json_data), ConnectionId=receiver_id)
     logger.info("sended")
     
-    return {"statusCode": 201}
+    return {"statusCode": 201, "body": "sended"}
